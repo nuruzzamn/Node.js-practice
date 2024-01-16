3bde520b4062e7a6118a9603c012d744837dbbaa
unknown <nur.mnzaman@gmail.com>
2024-01-16 23:34:31 +0600

    Initial commit

---
+++ localModule/index.js
@@ -0,0 +1,6 @@
+const s1 = require("./students");
+
+console.log(s1.getName());
+console.log(s1.getAge());
+console.log(s1.getLocation());
+console.log(s1.result);
---
+++ localModule/students.js
@@ -0,0 +1,18 @@
+const getName = () => {
+  return "zaman";
+};
+
+const getAge = () => {
+  return 27;
+};
+
+const getLocation = () => {
+  return "mirpur 1";
+};
+
+const result = 3.6;
+
+exports.getName = getName;
+exports.getAge = getAge;
+exports.getLocation = getLocation;
+exports.result = result;
