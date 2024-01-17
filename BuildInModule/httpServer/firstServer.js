ef290a7597200ae1f50c1c7c0b6ca6f046988ada
unknown <nur.mnzaman@gmail.com>
2024-01-17 15:07:30 +0600

    [feat] custom hostname

--- BuildInModule/httpServer/firstServer.js
+++ BuildInModule/httpServer/firstServer.js
@@ -1,9 +1,13 @@
 const http = require("http");
 
-const myServer = http
-  .createServer((req, res) => {
-    res.end("successfully create");
-  })
-  .listen(3000);
+const hostname = "127.0.0.1";
+const port = 3000;
+const myServer = http.createServer((req, res) => {
+  res.end("<h1> successfully create my first server </h1>");
+});
+
+const server = myServer.listen(port, hostname, () => {
+  console.log(`server is running at http://${hostname}:${port} `);
+});
 
 // console.log(http);
