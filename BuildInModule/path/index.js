70859cf79e78ba727b9418e3c958460dd79fb19c
unknown <nur.mnzaman@gmail.com>
2024-01-17 11:32:24 +0600

    [feat] path

---
+++ BuildInModule/path/index.js
@@ -0,0 +1,11 @@
+const path = require("path");
+
+// console.log(__dirname);
+// console.log(__filename);
+// const extensionName = path.extname("index.html");
+// console.log(extensionName);
+
+// const join = path.join(__dirname + "/newFile");
+const join = path.join(__dirname + "/../newFile");
+
+console.log(join);
