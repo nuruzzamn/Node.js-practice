1ae6ffa832f23eb884ba4652adeac77bca4d4aa1
unknown <nur.mnzaman@gmail.com>
2024-01-17 09:07:46 +0600

    [feat] overwrite name.txt file

--- BuildInModule/fsModule/index.js
+++ BuildInModule/fsModule/index.js
@@ -1,6 +1,6 @@
 const { log } = require("console");
 const fs = require("fs");
-fs.writeFile("name.txt", "zaman, nur", function (err) {
+fs.writeFile("name.txt", "zaman, nur, nuruzzaman, moon, shuvo", function (err) {
   if (err) {
     console.log(err);
   } else {
