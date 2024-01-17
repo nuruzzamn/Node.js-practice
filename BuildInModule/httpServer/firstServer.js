81b0b2888614f54bebbe5a00314d644ae887b264
unknown <nur.mnzaman@gmail.com>
2024-01-17 15:16:25 +0600

    [feat] with custom status code and content type

--- BuildInModule/httpServer/firstServer.js
+++ BuildInModule/httpServer/firstServer.js
@@ -3,7 +3,10 @@ const http = require("http");
 const hostname = "127.0.0.1";
 const port = 3000;
 const myServer = http.createServer((req, res) => {
-  res.end("<h1> successfully create my first server </h1>");
+  //in writeHead have two parameter (status code, content type)
+  res.writeHead(202, { "content-type": "text/plain" });
+  res.write("<h1> successfully create my first server </h1>");
+  res.end();
 });
 
 const server = myServer.listen(port, hostname, () => {
