e54193cda0dff0a13e39efe632057e2f808dc817
unknown <nur.mnzaman@gmail.com>
2024-01-17 07:40:25 +0600

    [feat] import as a object

--- localModule/index.js
+++ localModule/index.js
@@ -1,6 +1,9 @@
-const s1 = require("./students");
+// const s1 = require("./students");
+const { getName, getAge, getLocation } = require("./students");
 
-console.log(s1.getName());
-console.log(s1.getAge());
-console.log(s1.getLocation());
-console.log(s1.result);
+// console.log(s1.getName());
+// console.log(s1.getAge());
+// console.log(s1.getLocation());
+// console.log(s1.result);
+
+console.log(getName() + " age :" + getAge() + " location :" + getLocation());
